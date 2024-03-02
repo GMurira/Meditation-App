54156691e5e3a6af96ad50d202b13c1b7cb05a66
GMurira <nguyaimurira@gmail.com>
2024-03-02 23:10:37 +0300

    Working with Chips:

--- app/src/main/java/com/example/mediationapplication/MainActivity.kt
+++ app/src/main/java/com/example/mediationapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mediationapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -20,6 +22,9 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.ChipBorder
+import androidx.compose.material3.ChipColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -35,12 +40,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.ChipDefaults
 import com.example.mediationapplication.data.FILTER_CONTENT_LIST
+import com.example.mediationapplication.data.FilterComponent
+import com.example.mediationapplication.data.MEDIATION_TYPE_LIST
+import com.example.mediationapplication.data.MediationType
 import com.example.mediationapplication.ui.theme.MediationApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -127,5 +138,65 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun FilterOptionsComponent(){
         val filterOptions = FILTER_CONTENT_LIST
+        LazyRow(
+            modifier = Modifier.padding(top = 15.dp, start = 15.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            items(filterOptions.size){
+
+            }
+        }
+    }
+
+//    @Composable
+//    fun ChipComponent(filter: FilterComponent){
+//
+//        val contentColor = filter.color
+//        val filterText = filter.filterText
+//
+//       Chip(
+//           onClick = { /*TODO*/ },
+//           colors =,
+//           border =
+//       ) {
+//
+//       }
+//    }
+    @Composable
+    fun MeditationTypesComponent(){
+        val meditationOptions = MEDIATION_TYPE_LIST
+    LazyRow(
+        modifier = Modifier.padding(15.dp)
+    ){
+        items(meditationOptions.size){
+
+        }
+    }
+    }
+    @Composable
+    fun MeditationTypeComponent(
+        meditationTypes: MediationType){
+        
+        Card(
+            shape = RoundedCornerShape(14.dp),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier.padding(20.dp)
+            ){
+                Row (horizontalArrangement = Arrangement.SpaceEvenly){
+                    Chip(onClick = { /*TODO*/ }, colors = ChipDefaults.chipColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.White
+                        )
+                        , border =
+                    ) {
+                        Text(text = )
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mediationapplication/data/FilterContent.kt
+++ app/src/main/java/com/example/mediationapplication/data/FilterContent.kt
@@ -14,3 +14,45 @@ val FILTER_CONTENT_LIST = listOf(
     FilterComponent(Color.Yellow, R.string.text3),
     FilterComponent(Color.Magenta, R.string.text4)
 )
+
+/**
+ * Mediation Type Data class
+ */
+data  class MediationType(
+    val duration: String,
+    val teacher: String,
+    val title: String,
+    val description: String)
+
+val MEDIATION_TYPE_LIST = listOf(
+    MediationType(
+        duration = "45 mins",
+        teacher = "James Madchen",
+        title = "Love-Kind mediation",
+        description = "During Love kindness, you focus benevolent and loving energy towards yourself and others"
+    ),
+    MediationType(
+        duration = "34 mins",
+        teacher = "Kate London",
+        title = "Flower mediation",
+        description = "Outdoor concentration mediation, the object is a flower"
+    ),
+    MediationType(
+        duration = "64 mins",
+        teacher = "Henry Kissinger",
+        title = "Anger Managment",
+        description = "Learn to act within your bounds"
+    ),
+    MediationType(
+        duration = "34 mins",
+        teacher = "Kate London",
+        title = "Flower mediation",
+        description = "Outdoor concentration mediation, the object is a flower"
+    ),
+    MediationType(
+        duration = "34 mins",
+        teacher = "Kate London",
+        title = "Flower mediation",
+        description = "Outdoor concentration mediation, the object is a flower"
+    ),
+)
